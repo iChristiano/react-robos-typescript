fe24e3c2fa2353307bc744e6cf10a6014ca8b980
iChristiano <iChristiano.mail@gmail.com>
2022-02-04 14:52:53 +0100

    Updates

---
+++ precache-manifest.769d812796cc8fde8bd85f9f18f2460f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cd501547bdcaaca48c7",
+    "url": "/react-robos-typescript/static/css/main.1cd154da.chunk.css"
+  },
+  {
+    "revision": "2cd501547bdcaaca48c7",
+    "url": "/react-robos-typescript/static/js/main.2cd50154.chunk.js"
+  },
+  {
+    "revision": "560124736c961ad8e14a",
+    "url": "/react-robos-typescript/static/css/1.cd874d4b.chunk.css"
+  },
+  {
+    "revision": "560124736c961ad8e14a",
+    "url": "/react-robos-typescript/static/js/1.56012473.chunk.js"
+  },
+  {
+    "revision": "01965a3125fe554a215a",
+    "url": "/react-robos-typescript/static/js/runtime~main.01965a31.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robos-typescript/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b3b85a60209b801f7636a7e06190defe",
+    "url": "/react-robos-typescript/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/react-robos-typescript/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/react-robos-typescript/workbox-v3.6.3"});
+
+importScripts(
+  "/react-robos-typescript/precache-manifest.769d812796cc8fde8bd85f9f18f2460f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-robos-typescript/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
